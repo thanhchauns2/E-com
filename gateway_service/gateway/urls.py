87516290ac2324446a15519dbc7897f75972a05d
Nguyễn Thanh Châu <chaunguyenthanh7@outlook.com>
2023-04-17 21:05:59 +0700

    update url

--- gateway_service/gateway/urls.py
+++ gateway_service/gateway/urls.py
@@ -2,5 +2,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('create_cart/', views.create_cart, name='create_cart'),
+    path('register/', views.register, name='register'),
+    path('inititate_inventory/', views.inititate_inventory, name='inititate_inventory'),
+    path('add_product_to_inventory/', views.add_product_to_inventory, name='add_product_to_inventory'),
+    path('remove_product_from_inventory/', views.remove_product_from_inventory, name='remove_product_from_inventory'),
+    path('show_inventory/', views.show_inventory, name='show_inventory'),
+    path('add_product_to_cart/', views.add_product_to_cart, name='add_product_to_cart'),
+    path('remove_item_from_cart/', views.remove_item_from_cart, name='remove_item_from_cart'),
+    path('show_cart/', views.show_cart, name='show_cart'),
+    path('purchase/', views.purchase, name='purchase'),
+    path('track_order/', views.track_order, name='track_order'),
+    path('update_order/', views.update_order, name='update_order'),
 ]
