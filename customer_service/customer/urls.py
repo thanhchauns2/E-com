91332b610cbd8bcebf7efcb85e9a39eae86a6fe9
Nguyễn Thanh Châu <chaunguyenthanh7@outlook.com>
2023-04-17 18:14:51 +0700

    change user to customer

--- customer_service/customer/urls.py
+++ customer_service/customer/urls.py
@@ -1,4 +1,4 @@
-"""user_service URL Configuration
+"""customer_service URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
