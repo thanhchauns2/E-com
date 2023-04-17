e137e120eb9dd74a7971ca65d1215c0e4d2ccb8b
Nguyễn Thanh Châu <chaunguyenthanh7@outlook.com>
2023-04-17 09:47:43 +0700

    update

--- cart_service/cart/views.py
+++ cart_service/cart/views.py
@@ -29,3 +29,112 @@ def create_cart(request):
                 resp['status_code'] = '400'
                 resp['message'] = 'All fields are mandatory.'
     return HttpResponse(json.dumps(resp), content_type='application/json')
+
+def find_item_in_cart(cart, product_id):
+    items = cart.items
+    for item in items:
+        if item['product_id'] == product_id:
+            return item['quantity']
+    return 0
+
+
+@csrf_exempt
+def add_item_to_cart(request):
+    if request.method == 'POST':
+        if 'application/json' in request.META['CONTENT_TYPE']:
+            data = json.loads(request.body)
+
+            # Lấy thông tin cart và sản phẩm cần thêm
+            user_id = data.get('user_id')
+            cart_id = data.get('cart_id')
+            product_id = data.get('product_id')
+            quantity = data.get('quantity')
+
+            # Tìm cart theo cart_id và user_id
+            try:
+                cart = Cart.objects.get(cart_id=cart_id, user_id=user_id)
+            except Cart.DoesNotExist:
+                return HttpResponse('Cart not found', status=404)
+
+            quan = find_item_in_cart(cart=cart, product_id=product_id)
+            if quan == 0: # Thêm sản phẩm mới vào danh sách items của cart
+                items = cart.items
+                items.append({'product_id': product_id, 'quantity': quantity})
+                cart.items = items
+                cart.save()
+            else: # Chỉnh sửa số lượng
+                for item in items:
+                    if item['product_id'] == product_id:
+                        item['quantity'] = item['quantity'] + quantity
+
+            # Trả về thông tin của cart đã được cập nhật
+            response_data = {'cart_id': cart.cart_id, 'user_id': cart.user_id, 'items': cart.items}
+            return HttpResponse(json.dumps(response_data), content_type='application/json')
+
+    return HttpResponse('Invalid request', status=400)
+
+@csrf_exempt
+def remove_item_from_cart(request):
+    if request.method == 'POST':
+        if 'application/json' in request.META['CONTENT_TYPE']:
+            data = json.loads(request.body)
+
+            # Lấy thông tin cart và sản phẩm cần xoá
+            user_id = data.get('user_id')
+            cart_id = data.get('cart_id')
+            product_id = data.get('product_id')
+            quantity = data.get('quantity')
+
+            # Tìm cart theo cart_id và user_id
+            try:
+                cart = Cart.objects.get(cart_id=cart_id, user_id=user_id)
+            except Cart.DoesNotExist:
+                return HttpResponse('Cart not found', status=404)
+
+            quan = find_item_in_cart(cart=cart, product_id=product_id)
+            if quan == 0: # Sản phẩm không tồn tại trong cart
+                return HttpResponse('Item not found', status=404)
+            else: # Cập nhật số lượng hoặc xoá sản phẩm
+                for item in cart.items:
+                    if item['product_id'] == product_id:
+                        new_quantity = item['quantity'] - quantity
+                        if new_quantity > 0:
+                            item['quantity'] = new_quantity
+                        else:
+                            cart.items.remove(item)
+                
+                cart.save()
+
+            # Trả về thông tin của cart đã được cập nhật
+            response_data = {'cart_id': cart.cart_id, 'user_id': cart.user_id, 'items': cart.items}
+            return HttpResponse(json.dumps(response_data), content_type='application/json')
+
+    return HttpResponse('Invalid request', status=400)
+
+@csrf_exempt
+def clear_cart(request):
+    if request.method == 'POST':
+        if 'application/json' in request.META['CONTENT_TYPE']:
+            data = json.loads(request.body)
+
+            # Lấy thông tin cart và sản phẩm cần xoá
+            user_id = data.get('user_id')
+            cart_id = data.get('cart_id')
+
+            # Tìm cart theo cart_id và user_id
+            try:
+                cart = Cart.objects.get(cart_id=cart_id, user_id=user_id)
+            except Cart.DoesNotExist:
+                return HttpResponse('Cart not found', status=404)
+            
+            items = cart.items
+            for item in items:
+                cart.items.remove(item)
+            
+            cart.save()
+
+            # Trả về thông tin của cart đã được cập nhật
+            response_data = {'cart_id': cart.cart_id, 'user_id': cart.user_id, 'items': cart.items, 'message' : 'Cleared cart successfully.'}
+            return HttpResponse(json.dumps(response_data), content_type='application/json')
+
+    return HttpResponse('Invalid request', status=400)
