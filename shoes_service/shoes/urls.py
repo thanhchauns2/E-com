4efa67cb4baf7d990c43cd95ef4cc54b07aa905c
Nguyễn Thanh Châu <chaunguyenthanh7@outlook.com>
2023-05-04 16:50:05 +0700

    update clothes and shoes

--- shoes_service/shoes/urls.py
+++ shoes_service/shoes/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 urlpatterns = [
     path('create_brand/', views.create_brand, name='create_brand'),
-    path('create_clothing/', views.create_clothing, name='create_clothing'),
+    path('create_shoe/', views.create_shoe, name='create_shoe'),
     path('delete_brand/<int:brand_id>/', views.delete_brand, name='delete_brand'),
-    path('delete_clothing/<int:clothing_id>/', views.delete_clothing, name='delete_clothing'),
+    path('delete_shoe/<int:shoe_id>/', views.delete_shoe, name='delete_shoe'),
     path('search_brand/', views.search_brand, name='search_brand'),
-    path('search_clothing/', views.search_clothing, name='search_clothing'),
+    path('search_shoe/', views.search_shoe, name='search_shoe'),
 ]
